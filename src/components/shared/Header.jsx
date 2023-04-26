9bb096eaf905081891a12cfc9cbf7e49e0b57891
Nagib Mahfuz <120543584+nagibgithub@users.noreply.github.com>
2023-04-26 23:05:07 +0600

    install moment

--- src/components/shared/Header.jsx
+++ src/components/shared/Header.jsx
@@ -3,6 +3,7 @@ const Header = () => {
     return (
         <div className="text-center">
             <img className="mx-auto text-center" src="https://raw.githubusercontent.com/ProgrammingHero1/dragon-news-resources/main/assets/logo.png" alt="" />
+            <p>Journalism Without Fear or Favour</p>
         </div>
     );
 };

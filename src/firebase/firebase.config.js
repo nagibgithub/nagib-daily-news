9b95f6b1b034e249d8c0466a3ab08c308f63464b
Nagib Mahfuz <nmfuad.bd@gmail.com>
2023-04-26 19:59:20 +0600

    firebase Auth

---
+++ src/firebase/firebase.config.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyAJ4v_I3umQwjqq_1Xrn79F6FVYxTK8rkA",
+  authDomain: "nagib-news.firebaseapp.com",
+  projectId: "nagib-news",
+  storageBucket: "nagib-news.appspot.com",
+  messagingSenderId: "492414524642",
+  appId: "1:492414524642:web:19af2ab2653c3985a792bb"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);

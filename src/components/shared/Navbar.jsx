5002041650f6c18279d2e2e08a9501cd4bd66fb0
Nagib Mahfuz <120543584+nagibgithub@users.noreply.github.com>
2023-04-27 06:06:54 +0600

    navbar active route

--- src/components/shared/Navbar.jsx
+++ src/components/shared/Navbar.jsx
@@ -1,20 +1,23 @@
-import {Col, Row} from 'react-bootstrap';
+import {Button, Col, Row} from 'react-bootstrap';
 import Container from 'react-bootstrap/Container';
-import Navbar from 'react-bootstrap/Navbar';
-import {Link} from 'react-router-dom';
+import {Link, NavLink} from 'react-router-dom';
 
 const NavbarItem = () => {
     return (
-        <div>
-            <Navbar bg="light">
-                <Container>
-                    <Row>
-                        <Link to='/'><Navbar.Brand>Home</Navbar.Brand></Link>
-                        <Link to='/about'><Navbar.Brand>About</Navbar.Brand></Link>
-                        <Link to='/career'><Navbar.Brand>Career</Navbar.Brand></Link>
-                    </Row>
-                </Container>
-            </Navbar>
+        <div className='my-2'>
+            <Container>
+                <Row>
+                    <Col lg={4}></Col>
+                    <Col lg={4} className='d-flex'>
+                        <div className='my-2'><NavLink to='/' className={({isActive, isPending}) => isActive ? "bg-primary text-white text-decoration-none px-3 py-2" : isPending ? "pending" : "text-reset text-decoration-none px-3 py-2" }>Home</NavLink></div>
+                        <div className='my-2'><NavLink to='/about' className={({isActive, isPending}) => isActive ? "bg-primary text-white text-decoration-none px-3 py-2" : isPending ? "pending" : "text-reset text-decoration-none px-3 py-2" }>About Us</NavLink></div>
+                        <div className='my-2'><NavLink to='/career' className={({isActive, isPending}) => isActive ? "bg-primary text-white text-decoration-none px-3 py-2" : isPending ? "pending" : "text-reset text-decoration-none px-3 py-2" }>Career</NavLink></div>
+                    </Col>
+                    <Col lg={4}>
+                        <Button variant="secondary">Log In</Button>
+                    </Col>
+                </Row>
+            </Container>
         </div>
     );
 };

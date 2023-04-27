f9715f7de2bd448bcbae1f0fe583b812500c7d6d
Nagib Mahfuz <120543584+nagibgithub@users.noreply.github.com>
2023-04-27 06:16:34 +0600

    installing font awsome

--- src/components/shared/Navbar.jsx
+++ src/components/shared/Navbar.jsx
@@ -1,6 +1,8 @@
+import {faCircleUser} from '@fortawesome/free-solid-svg-icons';
+import {FontAwesomeIcon} from '@fortawesome/react-fontawesome';
 import {Button, Col, Row} from 'react-bootstrap';
 import Container from 'react-bootstrap/Container';
-import {Link, NavLink} from 'react-router-dom';
+import {NavLink} from 'react-router-dom';
 
 const NavbarItem = () => {
     return (
@@ -8,12 +10,13 @@ const NavbarItem = () => {
             <Container>
                 <Row>
                     <Col lg={4}></Col>
-                    <Col lg={4} className='d-flex'>
+                    <Col lg={4} className='d-flex justify-content-center'>
                         <div className='my-2'><NavLink to='/' className={({isActive, isPending}) => isActive ? "bg-primary text-white text-decoration-none px-3 py-2" : isPending ? "pending" : "text-reset text-decoration-none px-3 py-2" }>Home</NavLink></div>
                         <div className='my-2'><NavLink to='/about' className={({isActive, isPending}) => isActive ? "bg-primary text-white text-decoration-none px-3 py-2" : isPending ? "pending" : "text-reset text-decoration-none px-3 py-2" }>About Us</NavLink></div>
                         <div className='my-2'><NavLink to='/career' className={({isActive, isPending}) => isActive ? "bg-primary text-white text-decoration-none px-3 py-2" : isPending ? "pending" : "text-reset text-decoration-none px-3 py-2" }>Career</NavLink></div>
                     </Col>
-                    <Col lg={4}>
+                    <Col lg={4} className='text-end'>
+                        <FontAwesomeIcon className='text-secondary mx-2' size='2xl' icon={faCircleUser} />
                         <Button variant="secondary">Log In</Button>
                     </Col>
                 </Row>
